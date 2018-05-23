f5014ce24441556f5220cffa80b13e5aeb147ffc
Glazko <39458709+Glazko@users.noreply.github.com>
2018-05-23 22:20:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -454,6 +454,15 @@ client.on('message', message => {
                         });
                     }
                 
+                    var playervcs = new Discord.RichEmbed()
+                    .setColor(couleur[message.author.id].couleur)
+                    .setDescription(`_ _\n ${msg}`)
+                    .setThumbnail(`${image} `)
+                    .setAuthor(`${message.author.username}#${getvalueof.discriminator} • ${grade[message.author.id].grade} • ${getvalueof.id}`,`${message.author.avatarURL}`)
+                    .setFooter(`${message.guild} • ${message.guild.id}`)
+                    .setTimestamp()
+                    client.channels.findAll('name', 'vcs-sasuneki').map(a=>a.send(playervcs))
+                    client.channels.findAll('name', 'vcs').map(a=>a.send(playervcs))
 
                   //   client.on('message', (msg) => {
 
